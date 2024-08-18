455166d39712a0739fae2cec042d284dbbef2e5b
Carissa <yuriw@umich.edu>
2024-08-18 19:05:13 -0400

    added to header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import styled from 'styled-components' ;
 
+
 const Element = styled.div`
 `; 
 
 const ElementFlex = styled.div`
-
+    display: flex;
+    gap: 5vw;
 `; 
 
 const Logo = styled.div`
-
+    padding-right: 60vw;
+    font-size: 35px;
+    font-family: Kalam;
 `;
 
 const HeadStyle = styled.div`
+    background-color: #FF6A49;
 
 `;
 
 const Header = () => {
     return (
         <HeadStyle>
-            <Logo>ShortRoutes</Logo>
             <ElementFlex>
+            <Logo>ShortRoutes</Logo>
                 <Element>
                     Careers
                 </Element>
