a6ed5a64a5873628cb1a9a57ba1e774b59636bcb
Carissa <yuriw@umich.edu>
2024-08-19 14:53:27 -0400

    finished header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import Header from './components/Header';
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
+    <>
     <Header />
+    <Routes>
+        
+    </Routes>
+    </>
   );
 }
 
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import styled from 'styled-components' ;
+import Car from '../images/Car.svg'; 
 
 
 const Element = styled.div`
+    font-family: Itim;
+    font-size: 20px; 
+    color: white;
 `; 
 
 const ElementFlex = styled.div`
     display: flex;
+    align-items: center;
     gap: 5vw;
 `; 
 
 const Logo = styled.div`
-    padding-right: 60vw;
-    font-size: 35px;
+    padding-right: 45vw;
+    padding-left: 2vw;
+    font-size: 32px;
     font-family: Kalam;
+    color: white;
+    display: flex; 
+    gap: 1vw;
 `;
 
 const HeadStyle = styled.div`
@@ -25,7 +34,7 @@ const Header = () => {
     return (
         <HeadStyle>
             <ElementFlex>
-            <Logo>ShortRoutes</Logo>
+            <Logo>ShortRoutes <img src={Car} alt="Car" /></Logo>
                 <Element>
                     Careers
                 </Element>
