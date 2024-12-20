3ef3de027ea343b392424bdd22fb6715c23476c8
Carissa <yuriw@umich.edu>
2024-12-20 15:24:59 -0500

    enhanced CSS Pricing, added to confirmation

--- client/src/pages/Confirmation.tsx
+++ client/src/pages/Confirmation.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useRef, ChangeEvent, KeyboardEvent } from 'react';
 import { useNavigate } from "react-router-dom";
-import styled from 'styled-components' ;
+import styled from 'styled-components';
 
 const ConfirmStyle = styled.div`
     text-align: center;
@@ -14,8 +14,8 @@ const OneCode = styled.input`
     height: 7vw;
     border-radius: 8px;
     border: 1px solid #FF6A49;
-    color:  #FF6A49;
-    text-align:center;
+    color: #FF6A49;
+    text-align: center;
     font-size: 30px;
 `;
 
@@ -23,22 +23,88 @@ const CodeContainer = styled.div`
     display: flex;
     justify-content: space-between;
     width: 37vw;
-`
+`;
 
-const Confirmation = () => {
+const Confirmation: React.FC = () => {
+    const [values, setValues] = useState<string[]>(new Array(5).fill(''));
+    const inputRefs = useRef<HTMLInputElement[]>([]);
+    const navigate = useNavigate();
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
+        const { value } = e.target;
+
+        // Ensure only digits are entered
+        if (!/^\d*$/.test(value)) return;
+
+        const newValues = [...values];
+        newValues[index] = value.slice(-1); // Keep only the last digit
+        setValues(newValues);
+
+        // Move to the next input if a digit is entered
+        if (value && index < 4) {
+            inputRefs.current[index + 1]?.focus();
+        }
+
+        // If all boxes are filled, trigger a navigation or API call
+        if (newValues.every((v) => v !== '')) {
+            
+        }
+    };
+
+    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>, index: number) => {
+        if (e.key === 'Backspace') {
+            const newValues = [...values];
+            newValues[index] = '';
+            setValues(newValues);
+
+            // Move to the previous input if Backspace is pressed
+            if (index > 0 && values[index] === '') {
+                inputRefs.current[index - 1]?.focus();
+            }
+        }
+    };
+
+    const handlePaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const pasteData = e.clipboardData.getData('text').slice(0, 5);
+        const newValues = pasteData.split('').map((char) => (/^\d$/.test(char) ? char : ''));
+        setValues(newValues);
+
+        // Focus the appropriate input box based on the pasted data
+        newValues.forEach((char, i) => {
+            if (char && inputRefs.current[i]) {
+                inputRefs.current[i].focus();
+            }
+        });
+
+        // Check if verification code matches. If it does, then go to success page
+        if (newValues.every((v) => v !== '')) {
+            
+        }
+    };
+
     return (
         <ConfirmStyle>
             <div className="text fortypx">Creating Your Account...</div>
             <div className="text">Please enter the 5-digit code sent to your email</div>
             <CodeContainer>
-                <OneCode />
-                <OneCode />
-                <OneCode />
-                <OneCode />
-                <OneCode />
+                {values.map((_, index) => (
+                    <OneCode
+                        key={index}
+                        ref={(el) => {
+                            if (el) inputRefs.current[index] = el;
+                        }}
+                        type="text"
+                        maxLength={1}
+                        value={values[index]}
+                        onChange={(e) => handleChange(e, index)}
+                        onKeyDown={(e) => handleKeyDown(e, index)}
+                        onPaste={handlePaste}
+                    />
+                ))}
             </CodeContainer>
         </ConfirmStyle>
-    )
-}
+    );
+};
 
-export default Confirmation; 
+export default Confirmation;
--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -11,21 +11,25 @@ const PricingStyle = styled.div`
 
 const PricesContainer = styled.div`
     display: flex;
-    gap: 2%;
+    justify-content: space-between;
     text-align: center;
-    width: 70vw;
+    width: 58vw;
 `
 const PriceSection = styled.div`
     text-align: center;
 `
-
+const PlanButton = styled.button`
+    font-size: 15px;
+    margin-top: 5vh;
+`
 
 const PriceBox = styled.div`
     border: 1px solid #FF6A49;
     color: #FF6A49;
     border-radius: 5px;
     text-align: left;
-    width: 20vw;
+    width: 15vw;
+    padding-right: 2vw;
     height:60vh;
 `
 
@@ -34,9 +38,11 @@ const Pricing = () => {
     return (
         <PricingStyle>
             <div className="text fortypx">Pricing</div>
+            <br />
             <PricesContainer>
                 <PriceSection>
                     <div className="text twentypx">Free</div>
+                    <br />
                 <PriceBox>
                     <ul><li>10 free attempts every month</li>
                     <br />
@@ -49,23 +55,33 @@ const Pricing = () => {
                 </PriceSection>
                 <PriceSection>
                 <div className="text twentypx">$5/month</div>
+                <br />
                 <PriceBox>
                     <ul><li>Unlimited attempts every month</li>
+                    <br />
                     <li>Can input up to 15 locations</li>
+                    <br />
                     <li>Allows options for walking, car, and bike</li>
+                    <br />
                     <li>Comes with AI chatbot with limited input options</li></ul>
                 </PriceBox>
                 </PriceSection>
                 <PriceSection>
                 <div className="text twentypx">$10/month</div>
+                <br />
                 <PriceBox>
                 <ul><li>Unlimited attempts every month</li>
+                <br />
                     <li>Can input up to 20 locations</li>
+                    <br />
                     <li>Allows options for any mode of travel</li>
+                    <br />
                     <li>Comes with AI chatbot that further customizes the experience</li></ul>
                 </PriceBox>
                 </PriceSection>
             </PricesContainer>
+            <PlanButton>Choose a plan</PlanButton>
+            <div className="text">Cancel anytime!</div>
         </PricingStyle>
     );
 }
