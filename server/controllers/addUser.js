483518e87faa33c6090dc40843a701521b9b7b12
Carissa <yuriw@umich.edu>
2024-12-22 07:47:29 -0500

    add user function

--- server/controllers/addUser.js
+++ server/controllers/addUser.js
@@ -1,3 +1,19 @@
-const addUser = async (email, password) => {
-    
+const sendInfoDatabase = async (email, password) => {
+
 }
+
+const addUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        sendInfoDatabase(email, password);
+
+        res.status(200).json({ message: 'User added successfully' });
+
+    }catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: 'Failed to add the user' });
+    }
+
+}
+
+module.exports = {addUser};
