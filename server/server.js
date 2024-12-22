3e23b041144acfb7d6173175ca76fc10176ea318
Carissa <yuriw@umich.edu>
2024-12-21 23:15:19 -0500

    add user routes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const emailRoutes = require('./routes/emailRoutes');
+const userRoutes = require('./routes/addUserRoutes');
 const cors = require('cors'); 
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.use('/api', emailRoutes); 
+app.use('/api', addUserRoutes); 
 
 app.get('/', (req, res) => {
     res.send('Server is running!');
