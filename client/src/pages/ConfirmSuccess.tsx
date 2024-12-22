6d6079f811dc2ca6011ac7ede2e0b6746d173eff
Carissa <yuriw@umich.edu>
2024-12-21 19:33:00 -0500

    started success page

--- client/src/pages/ConfirmSuccess.tsx
+++ client/src/pages/ConfirmSuccess.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import { useNavigate } from "react-router-dom";
 import styled from 'styled-components' ;
 
-const SuccessStyle = styled.div``;
+const SuccessStyle = styled.div`    
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    `;
 
 const ConfirmSuccess = () => {
     return (
-        <></>
+        <SuccessStyle>
+            <div className="text fortypx">Account Creation Successful ✅</div>
+            <div className="text">Wait to be redirected back to home page..</div>
+        </SuccessStyle>
     )
 }
 
