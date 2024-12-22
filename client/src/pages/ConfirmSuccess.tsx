aba9559067dc000b9a6f274c6328879981212fdd
Carissa <yuriw@umich.edu>
2024-12-22 15:52:16 -0500

    api request success page

--- client/src/pages/ConfirmSuccess.tsx
+++ client/src/pages/ConfirmSuccess.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import {  useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 import styled from 'styled-components' ;
+import {  RootState } from '../redux/store';
 
 const SuccessStyle = styled.div`    
     text-align: center;
@@ -10,6 +13,24 @@ const SuccessStyle = styled.div`
     `;
 
 const ConfirmSuccess = () => {
+
+    const email = useSelector((state: RootState) => state.email);
+    const password = useSelector((state: RootState) => state.password);
+    const navigate = useNavigate();
+
+    const addUsertoDatabase = async () => {
+        try {
+            const response = await axios.post('http://localhost:5000/api/add-new-user', { email, password });
+            navigate("/"); 
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+            addUsertoDatabase();
+    }, []);
+
     return (
         <SuccessStyle>
             <div className="text fortypx">Account Creation Successful ✅</div>
