521818404c5eb0c2cc53080295e3e43c1964536d
yuriw99 <yuriw@umich.edu>
2025-01-02 13:41:32 -0500

    I fixed the error :)

--- server/controllers/timeMatrix.js
+++ server/controllers/timeMatrix.js
@@ -45,9 +45,10 @@ const getDistanceTwoPoints = async (location1, location2, travelMethod) => {
                 'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8'
             }
         });
-        const time = response.data.properties.summary.duration; // Duration in seconds
-        const directions = response.data.features[0].properties.segments[0].steps; // Directions steps (if you need them)
-        console.log("RESPONSE IS", response.data)
+        console.log("")
+        const time = response.data.features[0].properties.summary.duration; // Duration in seconds
+        const directions = response.data.features[0].properties.segments[0].steps // Directions steps (if you need them)
+        
         
         return { time, directions };
 
@@ -58,6 +59,7 @@ const getDistanceTwoPoints = async (location1, location2, travelMethod) => {
 
 //returns list of shortest distance nodes (for example [0, 1, 4, 2, 3]) as well as the total time to get to all of these nodes
 const applyAlgorithm = (matrix) => {
+    console.log(matrix)
     const n = matrix.length;
     const distances = Array(n).fill(Infinity);  // Shortest distance to each node
     const previous = Array(n).fill(null);  // To track the path
@@ -90,14 +92,16 @@ const applyAlgorithm = (matrix) => {
     }
 
     // Reconstruct the shortest path
-    const shortestPath = [];
+    const indices = [];
     let current = n - 1;  // Target node (change this for different destinations)
     while (current !== null) {
-        shortestPath.unshift(current);
+        indices.unshift(current);
         current = previous[current];
     }
-    const totalTime = distances[n - 1];
-    return {totalTime, shortestPath};
+    const time = distances[n - 1];
+    
+
+    return {time, indices};
 
 }
 
@@ -120,29 +124,32 @@ const findShortestRoute = async (req, res) => {
             let coordinate = await getCoordinate(locations[i]);
             coordinateList[i] = coordinate;
         }
-        const timeMatrix = null; 
-        const directionsMatrix = null;
+        const timeMatrix = []; 
+        const directionsMatrix = [];
         for (let i=0; i< coordinateList.length; i++){
+            timeMatrix[i] = [];
+            directionsMatrix[i] = [];
             for(let j=1; j<coordinateList.length; j++){
                 if(i == j){
                     timeMatrix[i][j] = Infinity; 
-                    directionsMatrix = [];
+                    directionsMatrix[i][j] = [];
                 }
                 else {
-                    const {time, directions} = getDistanceTwoPoints(coordinateList[i], coordinateList[j], transportList[i]);
+                    const {time, directions} = await getDistanceTwoPoints(coordinateList[i], coordinateList[j], transportList[i]);
                     timeMatrix[i][j] = time;
                     directionsMatrix[i][j] = directions;
                 }
             }
         }
         const {time, indices} = applyAlgorithm(timeMatrix);
+        console.log(time, indices)
         const locationsOrder = indices.map(index => locations[index]);
         const directionsOrder = getDirectionsList(directionsMatrix, indices)
         res.status(200).json({indices: indices, locations: locationsOrder, directions: directionsOrder, totalTime: time});
 
 
     } catch (error) {
-    //    console.log("findShortestDistance", error)
+        console.log("findShortestDistance", error)
         res.status(500).json({message: "failed distance matrix"})
     }
 }
