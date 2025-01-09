dfcf28f26ca3f7b679604edff0af507817fcc673
yuriw99 <yuriw@umich.edu>
2025-01-09 12:54:42 -0500

    routeslist

--- client/src/pages/RoutesList.tsx
+++ client/src/pages/RoutesList.tsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
+import axios from 'axios';
 import styled from 'styled-components';
+
 
 const RoutesList = () => {
 
+    const userEmail = useSelector((state: RootState) => state.email);
+    const user = userEmail.slice(0, userEmail.indexOf("@"));
+
+    useEffect(() => {
+        
+        }, []);
+
     return (
-        <></>
+        <>
+            <div className="text fortypx">Welcome Back, {user}</div>
+        </>
     )
 }
 
